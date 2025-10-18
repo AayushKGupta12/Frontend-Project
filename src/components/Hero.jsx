d072c4ab0e21a8e0645f18adaacbac5e5051cd0a
Aayush Kumar Gupta <aayushgupta120305@gmail.com>
2025-10-18 21:39:10 +0530

    Fix import case for LiveUser component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
-import LiveUser from "./Liveuser";
+import LiveUser from "./LiveUser";
 
 const Hero = () => {
   return (
